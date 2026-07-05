2e2ca4d3b0c0de9c2e485ae6b6e1fa4368273483
amirrezaghabeli <a.ghabeli@ravanertebat.com>
2026-07-05 13:55:27 +0330

    feat: add pie chart to home screen and update design size

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:money_manager/ui/home/home_screen.dart';
 
 void main() {
@@ -11,9 +12,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ScreenUtil(
-      options: const ScreenUtilOptions(
-        designSize: Size(360, 690),
+    return const ScreenUtil(
+      options: ScreenUtilOptions(
+        designSize: Size(360, 780),
         fontFactorByWidth: 2.0,
         fontFactorByHeight: 1.0,
         flipSizeWhenLandscape: true,
--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:money_manager/ui/core/constants/app_strings.dart';
 import 'package:money_manager/ui/core/theme/app_colors.dart';
 
@@ -9,26 +11,114 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: AppColors.backgroundColor,
-        title: Text(
-          AppString.appName,
-          style: TextStyle(
-            fontSize: 14,
-            fontWeight: FontWeight.w400,
-            color: AppColors.onSurfaceColor,
-          ),
-        ),
-      ),
-      body: _body(),
+      appBar: _appBar(context),
+      body: _body(context),
     );
   }
 
-  Widget _body() {
-    return Column(children: [
-        
+  AppBar _appBar(BuildContext context) {
+    return AppBar(
+      centerTitle: true,
+      backgroundColor: AppColors.backgroundColor,
+      title: Text(
+        AppString.appName,
+        style: TextStyle(
+          fontSize: context.sp(14),
+          fontWeight: FontWeight.w400,
+          color: AppColors.onSurfaceColor,
+        ),
+      ),
+    );
+  }
+
+  Widget _body(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          margin: EdgeInsets.symmetric(horizontal: context.w(16)),
+          decoration: BoxDecoration(
+            color: AppColors.backgroundColor,
+            borderRadius: BorderRadius.circular(16),
+            boxShadow: [const BoxShadow(color: AppColors.onSurfaceColor)],
+          ),
+          child: Row(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('BALANCE', style: TextStyle(fontSize: context.sp(16))),
+                  Row(
+                    children: [
+                      Text(
+                        '2400',
+                        style: TextStyle(
+                          fontSize: context.sp(16),
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        '\$',
+                        style: TextStyle(
+                          fontSize: context.sp(16),
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        _pieChart(),
       ],
+    );
+  }
+
+  AspectRatio _pieChart() {
+    const textStyle = TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.bold,
+      color: AppColors.backgroundColor,
+    );
+    return AspectRatio(
+      aspectRatio: 1.0,
+      child: Container(
+        // color: Colors.blue,
+        margin: const EdgeInsets.symmetric(horizontal: 16),
+        child: PieChart(
+          PieChartData(
+            sections: [
+              PieChartSectionData(
+                value: 10,
+                color: Colors.blue,
+                titleStyle: textStyle,
+              ),
+              PieChartSectionData(
+                value: 20,
+                color: Colors.amber,
+                titleStyle: textStyle,
+              ),
+              PieChartSectionData(
+                value: 30,
+                color: Colors.pink,
+                titleStyle: textStyle,
+              ),
+              PieChartSectionData(
+                value: 40,
+                color: Colors.green,
+                titleStyle: textStyle,
+              ),
+              PieChartSectionData(
+                value: 50,
+                color: Colors.red,
+                titleStyle: textStyle,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
